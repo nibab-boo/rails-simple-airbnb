cc1b5eb492fe6ef37511630045c5421f29f4cc52
Babin Bohora <bohorababin1@gmail.com>
2022-01-13 19:56:32 +0900

    flats controller default done

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,46 @@
 class FlatsController < ApplicationController
+  before_action :find_flat
+
+  def index
+    @flats = Flat.all
+  end
+
+  def show; end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    flat = Flat.new(build_flat)
+    if flat.save
+      flat_path(flat)
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @flat.update(build_flat)
+      flat_path(flat)
+    else
+      render :edit
+    end
+  end
+
+  def delete
+    @flat.destroy
+  end
+
+  private
+
+  def build_flat
+    params.require(:flat).permit(:name, :address, :description, :number_of_guests, :price_per_night)
+  end
+
+  def find_flat
+    @flat = Flat.find(params[:id])
+  end
 end
--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,2 +1,6 @@
 class Flat < ApplicationRecord
+  validates :name, :price_per_night, :number_of_guests, :address, :description, presence: true
+  validates :price_per_night, :number_of_guests, numericality: {
+    only_integer: true
+  }
 end
